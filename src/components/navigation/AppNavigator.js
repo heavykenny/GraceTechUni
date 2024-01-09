a269511d30e99ce58e2e1b01af675e8a664a08db
Kehinde Olawuwo <olawuwokenny@gmail.com>
2024-01-09 16:09:30 +0000

    Update AppNavigator - Refactor code to improve readability and maintainability. Update ForgotPasswordScreen - Add password reset functionality and error handling. The changes are necessary to allow users to reset their forgotten password.

--- src/components/navigation/AppNavigator.js
+++ src/components/navigation/AppNavigator.js
@@ -1,43 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { BottomNavigation } from 'react-native-paper';
+import React, {useEffect, useState} from 'react';
+import {BottomNavigation} from 'react-native-paper';
 import ProfileScreen from "../screens/ProfileScreen";
 import AttendanceScreen from "../screens/AttendanceScreen";
 import ModulesScreen from "../screens/ModulesScreen";
 import StackNavigator from "./StackNavigator";
-import { getUser } from "../../services/firebase/auth";
+import {getUser} from "../../services/firebase/auth";
 import AdminCreateCourseScreen from "../screens/AdminCreateCourseScreen";
 import AdminCreateUserScreen from "../screens/AdminCreateUserScreen";
 import Styles from "../../constants/styles";
-import {ActivityIndicator, View} from "react-native";
 import ModulesGradesScreen from "../screens/ModulesGradesScreen";
 import AdminCreateLecturerScreen from "../screens/AdminCreateLecturerScreen";
+import {ActivityIndicator, View} from "react-native";
 
 const AppNavigator = () => {
     const [index, setIndex] = useState(0);
     const [user, setUser] = useState({});
     const [isLoading, setIsLoading] = useState(true);
 
-    const userRoutes = [
-        { key: 'home', title: 'Home', focusedIcon: 'home', unfocusedIcon: 'home-outline' },
-        { key: 'attendance', title: 'Attendance', focusedIcon: 'calendar-check', unfocusedIcon: 'calendar-check-outline' },
-        { key: 'modules', title: 'Modules', focusedIcon: 'book', unfocusedIcon: 'book-outline' },
-        { key: 'profile', title: 'Profile', focusedIcon: 'account', unfocusedIcon: 'account-outline' },
-    ];
+    const userRoutes = [{
+        key: 'home',
+        title: 'Home',
+        focusedIcon: 'home',
+        unfocusedIcon: 'home-outline'
+    }, {
+        key: 'attendance',
+        title: 'Attendance',
+        focusedIcon: 'calendar-check',
+        unfocusedIcon: 'calendar-check-outline'
+    }, {key: 'modules', title: 'Modules', focusedIcon: 'book', unfocusedIcon: 'book-outline'}, {
+        key: 'profile',
+        title: 'Profile',
+        focusedIcon: 'account',
+        unfocusedIcon: 'account-outline'
+    },];
 
-    const adminRoutes = [
-        { key: 'admin_course', title: 'Course', focusedIcon: 'book-plus', unfocusedIcon: 'book-plus-outline' },
-        { key: 'admin_users', title: 'Students', focusedIcon: 'account-group', unfocusedIcon: 'account-group-outline' },
-        { key: 'admin_lecturer', title: 'Lecturer', focusedIcon: 'account-tie', unfocusedIcon: 'account-tie-outline' },
-        { key: 'admin_manage', title: 'Attendance', focusedIcon: 'account-cog', unfocusedIcon: 'account-cog-outline' },
-        { key: 'admin_profile', title: 'Profile', focusedIcon: 'account', unfocusedIcon: 'account-outline' },
-    ];
+    const adminRoutes = [{
+        key: 'admin_course',
+        title: 'Course',
+        focusedIcon: 'book-plus',
+        unfocusedIcon: 'book-plus-outline'
+    }, {
+        key: 'admin_users',
+        title: 'Students',
+        focusedIcon: 'account-group',
+        unfocusedIcon: 'account-group-outline'
+    }, {
+        key: 'admin_lecturer',
+        title: 'Lecturer',
+        focusedIcon: 'account-tie',
+        unfocusedIcon: 'account-tie-outline'
+    }, {
+        key: 'admin_manage',
+        title: 'Attendance',
+        focusedIcon: 'account-cog',
+        unfocusedIcon: 'account-cog-outline'
+    }, {key: 'admin_profile', title: 'Profile', focusedIcon: 'account', unfocusedIcon: 'account-outline'},];
 
 
-    const lecturerRoutes = [
-        { key: 'home', title: 'Home', focusedIcon: 'home', unfocusedIcon: 'home-outline' },
-        { key: 'lecturer_modules', title: 'Modules', focusedIcon: 'book', unfocusedIcon: 'book-outline' },
-        { key: 'lecturer_profile', title: 'Profile', focusedIcon: 'account', unfocusedIcon: 'account-outline' },
-    ];
+    const lecturerRoutes = [{
+        key: 'home',
+        title: 'Home',
+        focusedIcon: 'home',
+        unfocusedIcon: 'home-outline'
+    }, {
+        key: 'lecturer_modules',
+        title: 'Modules',
+        focusedIcon: 'book',
+        unfocusedIcon: 'book-outline'
+    }, {key: 'lecturer_profile', title: 'Profile', focusedIcon: 'account', unfocusedIcon: 'account-outline'},];
 
     const [routes, setRoutes] = useState(userRoutes);
 
@@ -88,13 +118,11 @@ const AppNavigator = () => {
         <ActivityIndicator size="large" color="#0000ff"/>
     </View>);
 
-    return (
-        <BottomNavigation
-            navigationState={{ index, routes }}
+    return (<BottomNavigation
+            navigationState={{index, routes}}
             onIndexChange={handleIndexChange}
             renderScene={renderScene}
-        />
-    );
+        />);
 };
 
 export default AppNavigator;
--- src/components/screens/ForgotPasswordScreen.js
+++ src/components/screens/ForgotPasswordScreen.js
@@ -3,32 +3,55 @@ import {Text, View} from 'react-native';
 import InputField from "../common/InputField";
 import Styles from "../../constants/styles";
 import CustomButton from "../common/CustomButton";
+import {resetPassword} from "../../services/firebase/auth";
+import MessageSnackBar from "../common/MessageSnackBar";
 
 const ForgotPasswordScreen = ({navigation}) => {
     const [email, setEmail] = useState('');
+    const [isMessageVisible, setIsMessageVisible] = useState(false);
+    const [message, setMessage] = useState('');
+    const [messageType, setMessageType] = useState('');
 
     const handlePasswordReset = () => {
-        console.log('Reset password for:', email);
+        if (!email) {
+            displayMessage('Please enter your email address', 'error');
+        } else {
+            resetPassword(email).then(r => {
+                displayMessage('Password reset email sent', 'success');
+                setTimeout(() => {
+                    navigation.navigate('Login');
+                }, 3000); // wait for 2 seconds
+            }).catch(e => {
+                displayMessage('Error resetting password', 'error');
+            });
+        }
     };
 
-    return (
-        <View style={Styles.container}>
-            <Text style={Styles.title}>Forgot Password</Text>
-            <InputField
-                label="Email"
-                placeholder={'user@example.com'}
-                value={email}
-                onChangeText={setEmail}
-                autoCapitalize="none"
-                keyboardType="email-address"
-            />
-            <View style={Styles.buttonContainer}>
-                <CustomButton icon={'lock-reset'} mode="contained" onPress={handlePasswordReset}>Reset Password</CustomButton>
-                <CustomButton icon={'login'} mode="text" onPress={() => navigation.navigate('Login')}>Remembered your
-                    password?</CustomButton>
-            </View>
+    const displayMessage = (msg, type) => {
+        setMessage(msg);
+        setMessageType(type);
+        setIsMessageVisible(true);
+    }
+
+    return (<View style={Styles.container}>
+        <MessageSnackBar visible={isMessageVisible} onDismiss={() => setIsMessageVisible(false)}
+                         message={message} type={messageType}/>
+        <Text style={Styles.title}>Forgot Password</Text>
+        <InputField
+            label="Email"
+            placeholder={'user@example.com'}
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize="none"
+            keyboardType="email-address"
+        />
+        <View style={Styles.buttonContainer}>
+            <CustomButton icon={'lock-reset'} mode="contained" onPress={handlePasswordReset}>Reset
+                Password</CustomButton>
+            <CustomButton icon={'login'} mode="text" onPress={() => navigation.navigate('Login')}>Remembered your
+                password?</CustomButton>
         </View>
-    );
+    </View>);
 };
 
 export default ForgotPasswordScreen;
